3b06d9e927f446e724334e80c814e76f387a5c25
jkressbach <jkressbach@utexas.edu>
2023-02-19 18:01:45 -0600

    added some placeholder code and comments

--- main/main.py
+++ main/main.py
@@ -20,7 +20,31 @@ def addCluster():
 # print ("there are" + current.getAvailability() + current.getName() + "available")
 
 """
+By Wednesday 2/22/23:
+
 output what hardware is being sold (based on mongodb cluster data)
 type name to show availability and capacity (xxx/yyy)
 type check out / check in xxx adn updated numbers are shown
 """
+
+#print list of all types of hardware in database, and what is available
+def currentHardware():
+        x = 0
+        #print ("There are xx/yy hardware available")
+
+
+
+currentHardware()
+print ("Which type of hardware would you like to interact with?")
+#input = next user input
+#print posssible commands, ask to input desired command
+#check for valid inoput, loop until valid
+#run hardwareSet.____ based on what command is
+command = ""
+match command:
+        case "check in":
+                0
+                #do this
+        case "check out":
+                0
+                #do that
