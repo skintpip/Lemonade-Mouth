0e2eeea9ee6a02972d707ed0c51e46d1511f2345
Dron <darrenbryant950@gmail.com>
2023-02-22 10:31:26 -0600

    Changed print to input

--- main/main.py
+++ main/main.py
@@ -1,20 +1,22 @@
 from pymongo import MongoClient
 import hardwareSet
 
-
-#use this cluster
-client = MongoClient("mongodb+srv://jkressbach:CIrRa3yVV8dhnfKT@cluster0.v1qezrw.mongodb.net/?retryWrites=true&w=majority")
+# use this cluster
+client = MongoClient(
+    "mongodb+srv://jkressbach:CIrRa3yVV8dhnfKT@cluster0.v1qezrw.mongodb.net/?retryWrites=true&w=majority")
 db = client.HardwareSet
 posts = db.HWSet1
-#print(collection)
+
+
+# print(collection)
 def addCluster():
-        # change this to have name, qty parameters and call hardwareSet constructor
-        post = {"Description": "Guitar Amps",
-                "Capacity": "75",
-                "Availability": "75",
-                }
-        post_id = posts.insert_one(post).inserted_id
-        print(post)
+    # change this to have name, qty parameters and call hardwareSet constructor
+    post = {"Description": "Guitar Amps",
+            "Capacity": "75",
+            "Availability": "75",
+            }
+    post_id = posts.insert_one(post).inserted_id
+    print(post)
 
 
 # print ("there are" + current.getAvailability() + current.getName() + "available")
@@ -27,24 +29,24 @@ type name to show availability and capacity (xxx/yyy)
 type check out / check in xxx adn updated numbers are shown
 """
 
-#print list of all types of hardware in database, and what is available
-def currentHardware():
-        x = 0
-        #print ("There are xx/yy hardware available")
 
+# print list of all types of hardware in database, and what is available
+def currentHardware():
+    x = 0
+    print("There are", "hardware available")
 
 
 currentHardware()
-print ("Which type of hardware would you like to interact with?")
-#input = next user input
-#print posssible commands, ask to input desired command
-#check for valid inoput, loop until valid
-#run hardwareSet.____ based on what command is
+input("Which type of hardware would you like to interact with?")
+# input = next user input
+# print possible commands, ask to input desired command
+# check for valid input, loop until valid
+# run hardwareSet.____ based on what command is
 command = ""
-match command:
-        case "check in":
-                0
-                #do this
-        case "check out":
-                0
-                #do that
+# match command:
+#         case "check in":
+#                 0
+#                 #do this
+#         case "check out":
+#                 0
+#                 #do that
