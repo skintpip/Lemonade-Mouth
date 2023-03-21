22b51594e42676625d2792d22778338fb38df929
Darren <darrenbryant950@gmail.com>
2023-03-20 21:14:47 -0500

    loginPage js file renders our page

--- client/src/loginPage.js
+++ client/src/loginPage.js
@@ -1,24 +1,41 @@
 // import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './mainPage.css';
-import React, { useEffect, useState, Component}from'react';
+import React, {useEffect, useState, Component} from 'react';
 
 /*
-TODO: Should be done by Wednesday 3/22
+to do: Should be done by Wednesday 3/22
 > render mainPage.html, render from the file or figure out how to copy the code onto render
 > Login button onClick handler, enter the user into the database or sign in, decrypt and encrypt may be necessary
 
  */
+
 //var perf =require('./mainPage.html');
-class loginPage extends React.Component {
-   // render(){
-   //    return (
-   //       <div>
-   //           {/* eslint-disable-next-line react/jsx-no-undef */}
-   //          <mainPage.html>
-   //          </mainPage.html>
-   //       </div>
-   //    );
-   // }
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <body>
+            <div class="login">
+                <form id="login" method="get" action="login.php">
+                    <div1>
+                        <label1>
+                            <b>User Name</b>
+                        </label1>
+                        <input type="text" name="Uname" id="Uname" placeholder="Username"></input>
+                        <br></br><br></br>
+                        <label2><b>Password
+                        </b>
+                        </label2>
+                        <input type="Password" name="Pass" id="Pass" placeholder="Password"></input>
+                        <br></br><br></br>
+                        <input type="button" name="log" id="log" value="Login"></input>
+                        <br></br><br></br>
+                    </div1>
+                </form>
+            </div>
+            </body>
+        );
+
+    }
 }
-export default loginPage;
+
+export default LoginPage;
