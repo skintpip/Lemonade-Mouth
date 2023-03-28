39514ada3edc62b90300de9cbe1f5fb06bd9873b
Darren <darrenbryant950@gmail.com>
2023-03-28 12:49:43 -0500

    Backend file now has code

---
+++ Server/backend.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify
+import certifi,pymongo,hardwareSet,cipher,user
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+# Create the server-side code (backend) for the front-end code that you created. As
+# part of this task, you are required to create the following functions:
+
+
+# This function queries the projectId and quantity from the URL and returns the project id and quantity to the front
+# end. The front end displays a pop - up message which says “ < qty > hardware checked in”
+@app.route('/checkedIn/<hwSet>/<projectId>/<qty>')
+def checkIn_hardware(hwSet,projectId, qty):
+    hwSet1 = hardwareSet.hardwareSet(hwSet)
+    ca = certifi.where()
+    client = pymongo.MongoClient(
+        "mongodb+srv://jkressbach:CIrRa3yVV8dhnfKT@cluster0.v1qezrw.mongodb.net/?retryWrites=true&w=majority",
+        tlsCAFile=ca)
+    db = client["HardwareSet"]
+    posts = db[hwSet]
+    hwSet1.mongo_check_in_item(posts,hwSet,qty)
+
+
+# This function queries the projectId and quantity from the URL and returns the
+# project id and quantity to the front end. The front end displays a pop-up message
+# which says “<qty> hardware checked out”
+@app.route('/checkedOut/<projectId>/<qty>')
+def checkOut_hardware(projectId, qty):
+    return {"projectID": [projectId], "checkedOut": [qty]}
+
+
+# This function queries the projectId from the URL and returns the project id to the
+# front end. The front end displays a pop-up message which says “Joined <projectId>”
+@app.route('/join/<projectId>')
+def joinProject(projectId):
+    return {"projectID": [projectId]}
+
+
+# This function queries the projectId from the URL and returns the project id to the
+# front end. The front end displays a pop-up message which says “Left <projectId>”
+@app.route('/leave/<projectId>')
+def leaveProject(projectId):
+    return {"projectID": [projectId]}
+
+
+@app.route('/test/<projectId>')
+def testPrint():
+    return {"members": [5555,5656,5657]}
+
+
+# @app.errorhandler('/404')
+# def not_found(e):
+#     return
+
+
+# app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    # app.run(debug=False)
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get("PORT", 5000))
