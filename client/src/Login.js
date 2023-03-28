fdb100626c750cd1b3ce2a873c1e0a1e5c789319
komalxb <112125106+komalxb@users.noreply.github.com>
2023-03-28 17:05:59 -0500

    Trying to edit login page

--- client/src/Login.js
+++ client/src/Login.js
@@ -14,7 +14,7 @@ export const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">email</label>
-                <input value={user} onChange={(e) => setUser(e.target.value)}type="user" placeholder="Enter username" id="user" name="user" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <button type="submit">Log In</button>
