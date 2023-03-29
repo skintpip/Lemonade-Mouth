e11f4e921bd2fb3d50967d4aa66d84f99d807e56
Pense2 <jackschilling@live.com>
2023-03-29 00:42:58 -0500

    Changed User Functions

--- Server/user.py
+++ Server/user.py
@@ -2,6 +2,12 @@ import pymongo
 import cipher
 
 
+client = pymongo.MongoClient(
+    "mongodb+srv://jkressbach:CIrRa3yVV8dhnfKT@cluster0.v1qezrw.mongodb.net/?retryWrites=true&w=majority", tlsCAFile=ca)
+db = client["Users"]
+userColl = db["Logins"]
+
+
 class User:
 
     # local, not on mongo (erase later)
@@ -13,30 +19,30 @@ class User:
     # mongo stuff
 
     # stores login for new user in database (password is encrypted)
-    def createNewUser(self, collection, username, password):
-        collection.insert_one({"Username": username, "Password": cipher.encrypt(password, 3, 1)}).inserted_id
+    def createNewUser(self, username, password):
+        userColl.insert_one({"Username": username, "Password": cipher.encrypt(password, 3, 1)}).inserted_id
         return username
 
     # attempts to perform a login with provided username and password
-    def loginExistingUser(self, collection, username, password):
-        if self.getPassword(self, collection, username) == password:
+    def loginExistingUser(self, username, password):
+        if self.getPassword(self, userColl, username) == password:
             return username
         return None
 
     # checks if a user already exists in the database
-    def doesUserExist(self, collection, username):
-        if collection.find({"Username": {"$in": username}}).count() > 0:
+    def doesUserExist(self, username):
+        if userColl.find({"Username": {"$in": username}}).count() > 0:
             return True
         return False
 
     # returns decrypted password of a given user
-    def getPassword(self, collection, username):
-        if self.doesUserExist(collection, username):
-            return cipher.decrypt(collection.find({"Username": username})[0].get("Password"), 3, 1)
+    def getPassword(self, username):
+        if self.doesUserExist(username):
+            return cipher.decrypt(userColl.find({"Username": username})[0].get("Password"), 3, 1)
         return None
 
     # returns encrypted password of a given user (for testing, should not be required in execution)
-    def getEncryptedPassword(self, collection, username):
-        if self.doesUserExist(collection, username):
-            return collection.find({"Username": username})[0].get("Password")
+    def getEncryptedPassword(self, username):
+        if self.doesUserExist(username):
+            return userColl.find({"Username": username})[0].get("Password")
         return None
